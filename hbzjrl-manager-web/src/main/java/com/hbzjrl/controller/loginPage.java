29a0a1cc749ea5ee85175c05a3fed60ae9b84120
dune.f <30288220+duneF@users.noreply.github.com>
2022-04-11 22:33:36 +0800

    从mysql 返回参数,准备展示show页面

--- hbzjrl-manager-web/src/main/java/com/hbzjrl/controller/loginPage.java
+++ hbzjrl-manager-web/src/main/java/com/hbzjrl/controller/loginPage.java
@@ -1,8 +1,11 @@
 package com.hbzjrl.controller;
 
+import com.hbzjrl.pojo.SSOT;
 import com.hbzjrl.pojo.UserDengLu;
+import com.hbzjrl.service.loginService;
 import org.apache.http.HttpRequest;
 import org.noggit.JSONUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 
 /***
  *@Author: Yc
@@ -24,10 +28,33 @@ import java.io.UnsupportedEncodingException;
 @RequestMapping(value = "/user")
 public class loginPage {
 
+    @Autowired
+    private loginService loginService;
+    private String username;
+    private String password;
+    private SSOT ssot;
+    private HashMap<String, String> map;
 
     @RequestMapping(value = "/login")
     @ResponseBody
-    public void login(UserDengLu userDengLu) {
-
+    public HashMap<String, String> login(UserDengLu userDengLu) {
+        ssot = new SSOT ();
+        map = new HashMap<> ();
+        username = userDengLu.getUsername();
+        password = userDengLu.getPassword();
+        System.out.println("CtrollerAAAAAAA"+userDengLu.getUsername()+userDengLu.getPassword());
+        map.put("username",username);
+        map.put("password", password);
+        userDengLu  = loginService.findUserByNameAndPasswd(map);
+        System.out.println("CtrollerBBBBBB"+userDengLu.toString());
+        if (userDengLu!=null){
+            System.out.println("CtrollerCCCCCCC"+userDengLu.toString());
+            ssot.setStatus(200);
+            System.out.println("Ctrollerddd"+userDengLu.toString());
+            map.put("status","200");
+            map.put("redirect","/show");
+        }
+        map.put("msg","账号密码错误");
+        return map;
     }
 }
--- hbzjrl-manager/hbzjrl-manager-pojo/src/main/java/com/hbzjrl/pojo/UserDengLu.java
+++ hbzjrl-manager/hbzjrl-manager-pojo/src/main/java/com/hbzjrl/pojo/UserDengLu.java
@@ -1,5 +1,8 @@
 package com.hbzjrl.pojo;
 
+import javax.xml.crypto.Data;
+import java.sql.Date;
+
 /***
  *@Author: Yc
  *@Date:2022/4/520:00
@@ -7,8 +10,18 @@ package com.hbzjrl.pojo;
  */
 public class UserDengLu {
     private String username;
-    private Integer password;
+    private String password;
     private String radioname;
+    private Integer login_id;
+    private String name;
+    private Integer tel;
+    private String address;
+    private Integer age;
+    private Integer sex;
+    private Data birthday;
+    private Date created;
+    private Date updated;
+
 
     public String getRadioname() {
         return radioname;
@@ -26,20 +39,101 @@ public class UserDengLu {
         this.username = username;
     }
 
-    public Integer getPassword() {
+    public String getPassword() {
         return password;
     }
 
-    public void setPassword(Integer password) {
+    public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Integer getLogin_id() {
+        return login_id;
+    }
+
+    public void setLogin_id(Integer login_id) {
+        this.login_id = login_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getTel() {
+        return tel;
+    }
+
+    public void setTel(Integer tel) {
+        this.tel = tel;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public Data getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Data birthday) {
+        this.birthday = birthday;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
     }
 
     @Override
     public String toString() {
         return "UserDengLu{" +
                 "username='" + username + '\'' +
-                ", password=" + password +
+                ", password='" + password + '\'' +
                 ", radioname='" + radioname + '\'' +
+                ", login_id=" + login_id +
+                ", name='" + name + '\'' +
+                ", tel=" + tel +
+                ", address='" + address + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                ", birthday=" + birthday +
+                ", created=" + created +
+                ", updated=" + updated +
                 '}';
     }
 }
